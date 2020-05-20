76fe63f219233ac0c4b72c770fb058802f1cd005
AidanRM6223 <63170558+AidanRM6223@users.noreply.github.com>
2020-05-19 17:54:49 -0700

    Update

---
+++ final/javascript files/script.js
@@ -0,0 +1,9 @@
+var loadTime;
+function loadPage() {
+    loadTime = setTimeout(showPage, 3000);
+}
+function showPage() {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("home").style.display = "block";
+  }
+}
