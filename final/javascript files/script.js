6f627d97c94562cc9a7ae38ccf46bb7cfa9ae203
AidanRM6223 <63170558+AidanRM6223@users.noreply.github.com>
2020-05-29 17:36:53 -0700

    Fixed dhruv's lists

--- final/javascript files/script.js
+++ final/javascript files/script.js
@@ -1,11 +1,30 @@
-
+// Aidan's Javascript
 var loadTime;
 function loadPage() {
   loadTime = setTimeout(showPage, 3000);
 }
 function showPage() {
   document.getElementById("loader").style.display = "none";
-  document.getElementById("home").style.visibility = "visible";
+  document.getElementById("main").style.display = "block";
+}
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function showDropdown() {
+  document.getElementById("dhruvs-Dropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dhruv-dropcontent");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
 }
 
 //Connors Section
